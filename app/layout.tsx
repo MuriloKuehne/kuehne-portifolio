117d7f59ce26418775ecb24689a72f5048242a83
Murilo <murkuehne@gmail.com>
2025-08-15 19:43:22 -0300

    Added Shadcnui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
@@ -24,18 +23,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   )
